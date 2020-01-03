6bd82aa1db3ee55a6c5db6b6ce1f35caa1db5505
Ronald <kyoberonald@gmail.com>
2020-01-03 05:47:32 +0000

    JS refresher+object+variables

---
+++ play.js
@@ -0,0 +1,34 @@
+var name = 'kyobe';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+    return (
+        'Name is ' +
+    userName +
+    ', age is ' +
+    userAge +
+    ' and the user has hobbies: ' +
+    userHasHobby
+        );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
+//    console.log(summarizeUser(name, age, hasHobbies));
+
+// var name = 'Max';
+// var age = 29;
+// var hasHobbies = true;
+
+// function summarizeUser(userName, userAge, userHasHobby) {
+//   return (
+//     'Name is ' +
+//     userName +
+//     ', age is ' +
+//     userAge +
+//     ' and the user has hobbies: ' +
+//     userHasHobby
+//   );
+// }
+
+// console.log(summarizeUser(name, age, hasHobbies));
